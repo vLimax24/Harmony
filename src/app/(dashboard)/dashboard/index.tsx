5b22f64cfd0007bc1a96f5d534f9d4aef9d83cd4
Limax <limax.workspace@gmail.com>
2024-07-02 09:01:27 +0200

    feat: sign in functionality

--- src/app/(dashboard)/dashboard/index.tsx
+++ src/app/(dashboard)/dashboard/index.tsx
@@ -1,15 +1,12 @@
 import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Page() {
   return (
     <View className="flex flex-1 items-center justify-center">
-      <Text>Welcome to Lou</Text>
-      <Link href={"/signIn"}>
-        <Text>Sign In</Text>
-      </Link>
+      <Text>Dashboard Route</Text>
+      <Link href={"/"}>Home</Link>
     </View>
   );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider, useAuth, ClerkLoaded } from "@clerk/clerk-expo";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 import { StatusBar } from "expo-status-bar";
+import * as SecureStore from "expo-secure-store";
 import { getLocales } from "expo-localization";
 import { I18n } from "i18n-js";
 import English from "../translations/en.json";
@@ -18,10 +19,29 @@ i18n.locale = getLocales()[0].languageCode;
 // to translate use i18n.t("key")
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL);
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
 export default function Layout() {
   return (
     <ClerkProvider
       publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
     >
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         <ClerkLoaded>
