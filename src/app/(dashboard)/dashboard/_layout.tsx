0fe9e42f28d74b2cf699409bfa2865c49c10a934
Limax <limax.workspace@gmail.com>
2024-07-03 19:29:21 +0200

    feat: notification permission in profile

--- src/app/(dashboard)/dashboard/_layout.tsx
+++ src/app/(dashboard)/dashboard/_layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Link, Tabs, Slot } from "expo-router";
-import { Pressable } from "react-native";
+import { View } from "react-native";
 import Navbar from "@/components/Navbar/Navbar";
 import { StatusBar } from "expo-status-bar";
 
@@ -13,7 +13,9 @@ export default function TabLayout() {
         backgroundColor="transparent"
         style="dark"
       />
-      <Slot />
+      <View className="flex-1 pt-12 px-5">
+        <Slot />
+      </View>
       <Navbar />
     </>
   );
--- src/app/(dashboard)/dashboard/profile/index.tsx
+++ src/app/(dashboard)/dashboard/profile/index.tsx
@@ -1,10 +1,31 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, Switch } from "react-native";
+import { api } from "../../../../../convex/_generated/api";
+import { useQuery } from "convex/react";
+import NotificationSetup from "@/components/Notifications/NotificationPermissionSwitch";
 
 const index = () => {
+  const myUser = useQuery(api.users.getMyUser);
+
+  const profileImage = myUser?.profileImage;
   return (
     <View className="flex-1">
-      <Text>Profile</Text>
+      {myUser && (
+        <View className="flex-row gap-5">
+          {profileImage && (
+            <Image
+              source={{ uri: profileImage }}
+              style={{ width: 50, height: 50 }}
+              className="rounded-full"
+            />
+          )}
+          <View>
+            <Text>{myUser.name}</Text>
+            <Text>{myUser.email}</Text>
+          </View>
+        </View>
+      )}
+      <NotificationSetup />
     </View>
   );
 };
