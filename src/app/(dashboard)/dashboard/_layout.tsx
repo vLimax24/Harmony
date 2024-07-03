49087fa29076bf76598d0fa5f70153e0b411fad7
Limax <limax.workspace@gmail.com>
2024-07-03 15:38:38 +0200

    fix: update StatusBar style

--- src/app/(dashboard)/dashboard/_layout.tsx
+++ src/app/(dashboard)/dashboard/_layout.tsx
@@ -3,10 +3,16 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Link, Tabs, Slot } from "expo-router";
 import { Pressable } from "react-native";
 import Navbar from "@/components/Navbar/Navbar";
+import { StatusBar } from "expo-status-bar";
 
 export default function TabLayout() {
   return (
     <>
+      <StatusBar
+        translucent={true}
+        backgroundColor="transparent"
+        style="dark"
+      />
       <Slot />
       <Navbar />
     </>
