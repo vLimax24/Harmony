18011b5f2884a64ad2e969bdf260ad82257f33a0
Limax <limax.workspace@gmail.com>
2024-07-24 23:16:45 +0200

    feat: update daily task display

--- src/app/(dashboard)/dashboard/index.tsx
+++ src/app/(dashboard)/dashboard/index.tsx
@@ -6,6 +6,8 @@ import { Button } from "@/components/ui/Button";
 import { router } from "expo-router";
 import { api } from "convex/_generated/api";
 import { useQuery } from "convex/react";
+import { Doc } from "convex/_generated/dataModel";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 export default function Page() {
   const { signOut } = useAuth();
@@ -14,15 +16,25 @@ export default function Page() {
     router.push("/welcome");
   };
 
-  const getAssignedTasksForDay = useQuery(
+  const getAssignedTasksForDay: Doc<"tasks">[] = useQuery(
     api.taskAssignments.getTaskAssignmentForDay
   );
 
   console.log(getAssignedTasksForDay);
   return (
     <View className="flex flex-1 items-center justify-center">
-      <Text>Dashboard Route</Text>
-      <Link href={"/"}>Home</Link>
+      {getAssignedTasksForDay && getAssignedTasksForDay.length <= 0 ? (
+        <Text className="font-bold text-textWhite">
+          No tasks assigned for today
+        </Text>
+      ) : (
+        getAssignedTasksForDay &&
+        getAssignedTasksForDay.map((task: Doc<"tasks">) => (
+          <Text key={task._id} className="font-bold text-textWhite">
+            {task.name}
+          </Text>
+        ))
+      )}
       <Button onPress={handleSignOut}>Sign Out</Button>
     </View>
   );
