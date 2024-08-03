65af33597e737c6a35e3a1645e3ac9d12a6a45fa
Limax <limax.workspace@gmail.com>
2024-08-03 22:03:18 +0200

    feat: refactor navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { View, TouchableOpacity, Text, Animated } from "react-native";
 import {
   Home,
   PieChart,
@@ -11,92 +11,123 @@ import { i18n } from "@/lib/i18n";
 import { GradientIcon } from "../ui/GradientIcon";
 import { GradientText } from "../ui/GradientText";
 
+// Utility function to handle navigation
+const navigateTo = (tab) => {
+  const routes = {
+    Home: "/dashboard",
+    Profile: "/dashboard/profile",
+    Groups: "/dashboard/groups",
+    Statistics: "/dashboard/statistics",
+  };
+  router.push(routes[tab]);
+};
+
+// Component for individual tab
+const NavTab = ({ tab, IconComponent, activeTab, onPress }) => {
+  const isActive = activeTab === tab;
+  const scaleValue = useRef(new Animated.Value(1)).current;
+  const rotateValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.parallel([
+        Animated.spring(scaleValue, {
+          toValue: isActive ? 1.1 : 1,
+          friction: 5,
+          useNativeDriver: true,
+        }),
+        Animated.spring(rotateValue, {
+          toValue: isActive ? 1 : 0,
+          friction: 5,
+          tension: 100, // Higher tension for more pronounced bounce
+          overshootClamping: true,
+          useNativeDriver: true,
+        }),
+      ]),
+      Animated.spring(rotateValue, {
+        toValue: 0, // Return to original position
+        friction: 5,
+        tension: 100, // Higher tension for quicker bounce
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, [isActive, scaleValue, rotateValue]);
+
+  const rotateInterpolate = rotateValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "10deg"], // Rotate by 10 degrees
+  });
+
+  return (
+    <TouchableOpacity onPress={onPress} className="items-center justify-center">
+      <Animated.View
+        style={{
+          transform: [{ scale: scaleValue }, { rotate: rotateInterpolate }],
+        }}
+      >
+        <GradientIcon
+          IconComponent={IconComponent}
+          isActive={isActive}
+          iconSize={24}
+        />
+      </Animated.View>
+      {isActive ? (
+        <GradientText
+          text={i18n.t(`Navbar.${tab.toLowerCase()}`)}
+          style={{ fontWeight: "bold" }}
+          className="text-sm"
+        />
+      ) : (
+        <Text className="font-bold text-sm text-textWhite">
+          {i18n.t(`Navbar.${tab.toLowerCase()}`)}
+        </Text>
+      )}
+    </TouchableOpacity>
+  );
+};
+
 const Navbar = () => {
   const pathname = usePathname();
   const [activeTab, setActiveTab] = useState("Home");
 
   useEffect(() => {
-    if (pathname === "/dashboard/profile") {
-      setActiveTab("Profile");
-    } else if (pathname === "/dashboard") {
-      setActiveTab("Home");
-    } else if (pathname === "/dashboard/groups") {
-      setActiveTab("Groups");
-    } else if (pathname === "/dashboard/statistics") {
-      setActiveTab("Statistics");
+    switch (pathname) {
+      case "/dashboard/profile":
+        setActiveTab("Profile");
+        break;
+      case "/dashboard":
+        setActiveTab("Home");
+        break;
+      case "/dashboard/groups":
+        setActiveTab("Groups");
+        break;
+      case "/dashboard/statistics":
+        setActiveTab("Statistics");
+        break;
+      default:
+        setActiveTab("Home");
+        break;
     }
   }, [pathname]);
 
-  const handleTabPress = (
-    tab: "Home" | "Groups" | "Statistics" | "Profile"
-  ) => {
-    router.push(
-      tab === "Profile"
-        ? "/dashboard/profile"
-        : tab === "Home"
-        ? "/dashboard"
-        : tab === "Groups"
-        ? "/dashboard/groups"
-        : tab === "Statistics"
-        ? "/dashboard/statistics"
-        : "/dashboard"
-    );
-  };
-
-  const renderText = (text, isActive) => {
-    return isActive ? (
-      <GradientText text={text} style={{ fontWeight: "bold" }} />
-    ) : (
-      <Text className="font-bold text-sm text-textWhite">{text}</Text>
-    );
-  };
+  const tabs = [
+    { tab: "Home", IconComponent: Home },
+    { tab: "Groups", IconComponent: UsersRound },
+    { tab: "Statistics", IconComponent: PieChart },
+    { tab: "Profile", IconComponent: CircleUserRound },
+  ];
 
   return (
     <View className="flex-row items-center justify-around pt-4 pb-6 bg-backgroundShade">
-      <TouchableOpacity
-        onPress={() => handleTabPress("Home")}
-        className="items-center justify-center"
-      >
-        <GradientIcon
-          IconComponent={Home}
-          isActive={activeTab === "Home"}
-          iconSize={24}
+      {tabs.map(({ tab, IconComponent }) => (
+        <NavTab
+          key={tab}
+          tab={tab}
+          IconComponent={IconComponent}
+          activeTab={activeTab}
+          onPress={() => navigateTo(tab)}
         />
-        {renderText(i18n.t("Navbar.home"), activeTab === "Home")}
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => handleTabPress("Groups")}
-        className="items-center justify-center"
-      >
-        <GradientIcon
-          IconComponent={UsersRound}
-          isActive={activeTab === "Groups"}
-          iconSize={24}
-        />
-        {renderText(i18n.t("Navbar.groups"), activeTab === "Groups")}
-      </TouchableOpacity>
-      {/* <TouchableOpacity
-        onPress={() => handleTabPress("Statistics")}
-        className="items-center justify-center"
-      >
-        <GradientIcon
-          IconComponent={PieChart}
-          isActive={activeTab === "Statistics"}
-          iconSize={24}
-        />
-        {renderText(i18n.t("Navbar.statistics"), activeTab === "Statistics")}
-      </TouchableOpacity> */}
-      <TouchableOpacity
-        onPress={() => handleTabPress("Profile")}
-        className="items-center justify-center"
-      >
-        <GradientIcon
-          IconComponent={CircleUserRound}
-          isActive={activeTab === "Profile"}
-          iconSize={24}
-        />
-        {renderText(i18n.t("Navbar.profile"), activeTab === "Profile")}
-      </TouchableOpacity>
+      ))}
     </View>
   );
 };
